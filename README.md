1f6b0303ce44bb3870c0d0cee0cf415e96f071d1
sheepoX <peng.jz.xu@gmail.com>
2017-08-03 16:27:30 -0400

    Updated index.js to include podcast templates
    Allow users to create and execute podcast template files

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # podbot
-A Discord bot designed to record voice chat audio, aimed at recording a specific podcast.
+A Discord bot designed to record voice chat audio, aimed at recording a specific podcast. Originally created by Fiddleskins. Edited by sheepoX to include podcast template files.
 
-Click [here](https://discordapp.com/oauth2/authorize?client_id=270724051234717698&scope=bot&permissions=133237760) to add my instance of it.
-
-### GIB PODCAST NAO
-Someone has actually adapted this project and wrapped it up in a user-friendly package.  
-Go take a look at [podframe](https://podframe.com/) if you want to be able to just add a bot
-and start recording.
-
-(This is made by some chap by the name of _leanon over on this [reddit thread](https://www.reddit.com/r/discordapp/comments/6733u8/i_made_a_discord_bot_which_lets_you_record_a/?st=j1yqjsv8&sh=d25c1299))
+Click [here](https://discordapp.com/oauth2/authorize?client_id=334496650510991371&scope=bot&permissions=133237760) to add my instance of it.
 
 ### The current commands are:
-- `/podon` - The bot starts recording
-- `/podoff` - The bot stops recording
+- `/create_podcast TemplateTitle` - Create podcast template csv
+- `/add_topic |Topic|Link|StartTime` - Add topic to template with link for reference and a approximated start time
+- `/finish_podcast` - End podcast template creation
+- `/podon PodcastTitle` - The bot starts recording with or without a template loaded as PodcastTitle. If no template loaded, PodcastTitle will only act as a title
+- `/next_topic` - Move onto next topic in template while podon
+- `/podoff` - The bot stops recording and creates podcast guide file if template loaded
+
+Podcast guide file will be saved to `podbot\podcasts\PodcastTitle-timestamp\PodcastTitle-Guide.csv`
   
-The bot will generate audio fragments saved to `podbot\podcasts\<channelId-timestamp>.opus_string`. 
+The bot will generate audio fragments saved to `podbot\podcasts\<PodcastTitle-timestamp>.opus_string`. 
 These will need to be first decoded into PCM by doing:
 
 `node decodeOpus.js <name of the folder containing the podcast session you want processed>`
@@ -29,7 +28,5 @@ It's worth noting that you'll need to host the bot yourself if you want access t
 
 ## You want your own?
 Then clone this repo and do the thing with the discord and the applications.
-
-You will need to place a text file called 'token' in the root of the bot containing just the login token for your bot.
 
 You will also need to place a text file called 'controllers' in the same place containing whitespace separated user IDs in order for anyone to be able to control the bot.
--- index.js
+++ index.js
@@ -3,30 +3,79 @@
 const fs = require('fs');
 const path = require('path');
 const Discord = require('discord.js');
+const parse = require('csv-parse');
+const csvWriter = require('csv-write-stream');
 
-const TOKEN = fs.readFileSync('./token', 'utf8').trim(); // Trim because linux
+//Discord Tokens
+const TOKEN = 'MzQwMjEwOTY2MjAzNzI3ODcy.DFvOJg.h7n_xsoFFknZ_Y0JGgYO4E84q7I';
 const CONTROLLER_IDS = [];
 
+//Globals
+var startIndex = -1;
+var trueIndex = -1;
+var timer = 0;
+var startTime = [];
+var topic = [];
+var link = [];
+var ms = [];
+var intervalID;
+var timeoutID;
+var podTitle = null;
+var podcastName = null;
+var templateLoaded = false;
+
+// Extract Keywords from Discord Inputs
+function locations(substring,string){
+    var a=[], i=-1;
+    while((i=string.indexOf(substring,i+1)) >= 0) a.push(i);
+    return a;
+}
+
+function extractStrings(locations,string){
+    var a=[];
+    for(var i=0; i < locations.length; i++){
+        if (i !== locations.length - 1){
+            a.push(string.substr(locations[i]+1,locations[i+1]-locations[i]-1).trim());
+        }
+        else{
+            a.push(string.substr(locations[i]+1).trim());
+        }
+    }
+    return a;
+}
+
+//Convert seconds to MM:SS format
+function secToTime(duration) {
+        let minutes = Math.floor(duration / 60);
+        let seconds = duration - minutes * 60;
+    
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+        return minutes + ":" + seconds;
+}
+
 // Populate controller_ids
-fs.readFile('./controllers', 'utf8', (err, data) => {
+
+fs.readFile('controllers.txt', 'utf8', (err, data) => {
 	if (!err) {
 		[].push.apply(CONTROLLER_IDS, data.split(/\s+/));
 	}
 });
-
 
 class Podbot {
 	constructor(token) {
 		this.client = new Discord.Client();
 		this.commandCharacter = '/';
 		this.podcastsPath = Podbot._makePodcastsDirectory();
+        this.templatePath = Podbot._makeTemplatesDirectory();
 
 		this._controllerUsers = new Set();
-
+        
 		this._voiceConnections = new Map();
 		this._voiceReceivers = new Map();
 		this._writeStreams = new Map();
-
+        
 		this.client.on('ready', this._onReady.bind(this));
 
 		this.client.on('message', this._onMessage.bind(this));
@@ -47,15 +96,74 @@ class Podbot {
 	}
 
 	_onMessage(message) {
+        
+        const start_podcast_reg = /^podon .*/;
+		const create_podcast_reg = /^create_podcast .*/;
+        const add_section_reg = /^add_topic .*/;
+        
 		if (message.content.charAt(0) === this.commandCharacter) {
-			switch (message.content.slice(1)) {
-				case 'podon':
-					this._podon(message.member);
-					break;
-				case 'podoff':
-					this._podoff(message.member);
-					break;
-			}
+            if (start_podcast_reg.test(message.content.slice(1))){
+                podTitle = message.content.slice(1).substr(6).trim();
+                this._podon(message.member,podTitle, message);
+            }
+            else if (message.content.slice(1) === 'podoff'){
+                this._podoff(message.member);
+            }
+            else if (create_podcast_reg.test(message.content.slice(1))){
+                if (this._checkMemberHasPermissions(message.member)) {
+                    let title = message.content.slice(1).substr(15).trim() + '.csv';
+                    let outputPath = path.join(this.templatePath,title);
+                    this.writer = 
+                        csvWriter({headers: ["Topic","Link","Time_Start"]});
+                    this.writer.pipe(fs.createWriteStream(outputPath));
+                    message.reply('Insert Podcast Sections: /add_topic |topic|link|startTime');
+                    message.reply('When finished: /finish_podcast');
+                }
+                else{
+                    message.reply("No Permission");
+                }
+            }
+            else if (add_section_reg.test(message.content.slice(1))){
+                if (this._checkMemberHasPermissions(message.member)) {
+                    if (this.writer !== null){
+                        var newTopic = extractStrings(locations("|",message.content.slice(1))
+                            ,message.content.slice(1));
+                        this.writer.write(newTopic);
+                        message.reply(newTopic[0] + ' Section Added!');
+                    }
+                    else{
+                        message.reply('Create a Podcast First!');
+                    }
+                }
+                else{
+                    message.reply("No Permission");
+                }
+            }
+            else if (message.content.slice(1) === 'finish_podcast'){
+                if (this._checkMemberHasPermissions(message.member)) {
+                    this.writer.end;
+                    this.writer = null;
+                    message.reply('Podcast Template Finished')
+                }
+                else{
+                    message.reply("No Permission");
+                }
+            }
+            else if (message.content.slice(1) === 'next_topic'){
+                if (this._checkMemberHasPermissions(message.member)) {
+                    if (!templateLoaded){
+                        message.reply("No Template Loaded")
+                        return;
+                    }
+                    message.reply('Current Topic: '+ topic[i] + ' - ' + link[i]);
+                    let curTime = secToTime(timer);
+                    startTime.push(curTime);
+                    trueIndex++;
+                }
+                else{
+                    message.reply("No Permission");
+                }
+            }
 		}
 	}
 
@@ -77,20 +185,79 @@ class Podbot {
 		}
 	}
 
-	_podon(member) {
+	_podon(member, podcastTitle, message) {
 		if (!member) {
+            message.reply('Not Member');
 			return;
 		}
 		if (!this._checkMemberHasPermissions(member)) {
-			return;
+			message.reply('No Permission');
+            return;
 		}
 		if (!member.voiceChannel) {
-			return;
+			message.reply('Incorrect Voice Channel');
+            return;
 		}
+        
+        let podTitleCSV = podcastTitle + '.csv';
+        let readPath = path.join(this.templatePath,podTitleCSV);
+        
+        podcastName = `${podcastTitle}-${Date.now()}`;
+        
+        fs.readFile(readPath, function (err, data) {
+            if (!err){
+                parse(data, {columns: true, trim: true}, function(err, rows){
+                    if (!err){
+                        templateLoaded = true;
+                        intervalID = setInterval(function(){
+                            timer++;},1000);
+                            
+                        for(var i = 0; i < rows.length; i++){
+                          topic.push(rows[i].Topic);
+                          link.push(rows[i].Link);
+                          var start_time = rows[i].Time_Start;
+                          var min = parseInt(start_time.substr(0,2));
+                          var sec = parseInt(start_time.substr(3));
+                          ms.push((min*60000)+(sec*1000));
+                        }
+                        
+                        startIndex = 0;
+                        trueIndex = 0;
 
-		let podcastName = `${member.voiceChannelID}-${Date.now()}`;
-		Podbot._makeDirectory(path.join(this.podcastsPath, podcastName));
-
+                        function doSetTimeout(i){
+                            timeoutID = setTimeout(function() { 
+                                if (trueIndex == startIndex && trueIndex !== -1 && startIndex !== -1){
+                                    message.reply('Current Topic: '+ topic[i] + ' - ' + link[i]);
+                                    startIndex++;
+                                    trueIndex++;
+                                    let curTime = secToTime(timer);
+                                    startTime.push(curTime);
+                                }
+                                else{
+                                    startIndex++;
+                                }
+                            }, ms[i]);
+                        }
+                        
+                        for (var i = 0; i < topic.length; i++){    
+                            doSetTimeout(i);
+                        }
+                    }
+                    else{
+                        message.reply("File Error");
+                        message.reply("No Template Loaded");
+                        return;
+                    }});
+                }
+                else{
+                    message.reply("File Not Found");
+                    message.reply("No Template Loaded");
+                    return;
+                }});
+        
+        message.reply('Starting');
+        Podbot._makeDirectory(path.join(this.podcastsPath, podcastName));
+        
 		member.voiceChannel.join().then((voiceConnection) => {
 			this._voiceConnections.set(member.voiceChannelID, voiceConnection);
 			let voiceReceiver = voiceConnection.createReceiver();
@@ -110,7 +277,7 @@ class Podbot {
 					if (hexString === 'f8fffe') {
 						return;
 					}
-					let outputPath = path.join(this.podcastsPath, podcastName, `${user.id}-${Date.now()}.opus_string`);
+					let outputPath = path.join(this.podcastsPath, podcastName, `${podcastTitle}-${Date.now()}.opus_string`);
 					writeStream = fs.createWriteStream(outputPath);
 					this._writeStreams.set(user.id, writeStream);
 				}
@@ -127,13 +294,46 @@ class Podbot {
 		if (!this._checkMemberHasPermissions(member)) {
 			return;
 		}
-
+        
+        console.log("Ending");
+        
 		if (this._voiceReceivers.get(member.voiceChannelID)) {
 			this._voiceReceivers.get(member.voiceChannelID).destroy();
 			this._voiceReceivers.delete(member.voiceChannelID);
 			this._voiceConnections.get(member.voiceChannelID).disconnect();
 			this._voiceConnections.delete(member.voiceChannelID);
 		}
+        
+        podcastName = null;
+        
+        if (!templateLoaded){
+            return;
+        }
+        
+        let podGuidePath = path.join(this.podcastsPath,podcastName,podTitle+'-Guide.csv');
+        var guideWriter = csvWriter({headers: ["Topic","Link","Time_Start"]});
+        guideWriter.pipe(fs.createWriteStream(podGuidePath));
+        
+        
+        for(var i = 0; i < startTime.length; i++){
+            let guideInput = [topic[i],link[i],startTime[i]];
+            guideWriter.write(guideInput);
+        }
+        
+        guideWriter.end();
+        guideWriter = null;
+        
+        templateLoaded = false;
+        clearInterval(intervalID);
+        clearTimeout(timeoutID);
+        timer = 0;
+        startIndex = -1;
+        trueIndex = -1;
+        topic = [];
+        link = [];
+        ms = [];
+        podTitle = null;
+        
 	}
 
 	_checkMemberHasPermissions(member) {
@@ -144,6 +344,12 @@ class Podbot {
 
 	static _makePodcastsDirectory() {
 		let dir = path.join('.', 'podcasts');
+		Podbot._makeDirectory(dir);
+		return dir;
+	}
+    
+    static _makeTemplatesDirectory() {
+		let dir = path.join('.', 'templates');
 		Podbot._makeDirectory(dir);
 		return dir;
 	}
